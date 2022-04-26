8f6a562f3c18d81251c1851660bba34afb7f9119
Alex Brown <alex.brown.r@gmail.com>
2022-04-26 15:09:03 +0800

    Update image name

--- web/secure-quiche-lorraine/challenge/start_docker.sh
+++ web/secure-quiche-lorraine/challenge/start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t my-first-site-80 . && \
-docker run -it -p 80:80 --rm --name my-first-site-container my-first-site-80
+docker build -t secure-quich-lorraine-80 . && \
+docker run -it -p 80:80 --rm --name secure-quich-lorraine-container secure-quich-lorraine-80
